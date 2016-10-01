76341870b1b9d7ac2e7b9e474dd859f3b68e78f6
tkantusch <tobiaskantusch@online.de>
2016-10-02 00:23:19 +0200

    Wechsel zur Endung .php
    HTML zu PHP geändert um das Menü und den Footer auszulagern und damit zu zentralisieren und zu vereinfachen.

---
+++ htdocs/einsatzinformationen/osr-berichte.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="Internetseiten der Schiedsrichterinnen und Schiedsrichter des BeTTV">
+	<meta name="author" content="" />
+
+	<title>OSR-Berichte | Die Schiedsrichterinnen und Schiedsrichter des BeTTV</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link href="/css/modern-business.css" rel="stylesheet">
+
+	<!-- Custom Fonts -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+
+<body>
+
+	<?php include ("../menueleiste.php"); ?>
+
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">OSR-Berichte
+
+                </h1>
+
+
+            </div>
+        </div>
+
+        <hr />
+
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Zu jedem Spiel, das Ihr als OSR leitet, ist ein OSR-Bericht zu erstellen.
+                    Auf dieser Seite sind alle Informationen zusammengefasst, die dafür nötig sind.
+                </p>
+
+                <h2 id="kurzanleitung">Kurzanleitung</h2>
+
+                <p>Folgende Schritte sind für einen korrekten OSR-Bericht vorzunehmen:</p>
+
+                <ol>
+                  <li>OSR-Bericht des DTTB herunterladen (<a href="#download">Anleitung</a>)</li>
+                  <li>OSR-Bericht korrekt benennen (<a href="#name">Anleitung</a>)</li>
+                  <li>OSR-Bericht ausfüllen (<a href="#fill">Anleitung</a>)</li>
+                  <li>OSR-Bericht abspeichern (<a href="#save">Anleitung</a>)</li>
+                  <li>OSR-Bericht per Mail versenden (<a href="#send">Anleitung und Adressen</a>)</li>
+                </ol>
+
+                <h2 id="download">OSR-Bericht des DTTB herunterladen</h2>
+
+                <p>Ladet den für die Spielklasse passenden OSR-Bericht herunter.</p>
+
+                <h3 id="osr-berichte">OSR-Berichte</h3>
+
+                <table class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th style="text-align: left">Name</th>
+                      <th style="text-align: left">Beschreibung</th>
+                      <th style="text-align: left">Stand</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td style="text-align: left"><a href="/downloads/osr-berichte/BL_OSR-Bericht.pdf">OSR-Bericht BL</a></td>
+                      <td style="text-align: left">Ausfüllhilfe und Empfänger siehe <a href="osr-berichte.html">OSR-Berichte</a>.</td>
+                      <td style="text-align: left">1. September 2016</td>
+                    </tr>
+                    <tr>
+                      <td style="text-align: left"><a href="/downloads/osr-berichte/RL-OL_OSR-Bericht.pdf">OSR-Bericht RL/OL</a></td>
+                      <td style="text-align: left">Ausfüllhilfe und Empfänger siehe <a href="osr-berichte.html">OSR-Berichte</a>.</td>
+                      <td style="text-align: left">1. September 2016</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <p>Speichert den OSR-Bericht am besten auf die Festplatte und füllt ihn dort aus.
+                    Wenn der OSR-Bericht direkt im Browser geöffnet wird, geht zurück, klickt den Link mit der rechten Maustaste an und wählt “Ziel speichern unter”.
+                </p>
+
+                <p>In anderen Worten: “Bei mir selbst erscheint die gleiche Meldung […] Allerdings bestätige ich dann rechts oben nochmal das Öffnen und wähle den Adobe-Reader aus. Dann öffnet sich der OSR-Bericht.”</p>
+
+                <h2 id="name">OSR-Bericht korrekt benennen</h2>
+
+                <p>Benennt den heruntergeladenen Bericht bitte nach folgendem Namensschema:</p>
+
+                <div class="highlighter-rouge">
+                    <pre class="highlight">
+                        <code>"OSR"_Nennung der Spielklasse_Damen/Herren_Spielnummer_Begegnung.pdf</code>
+                    </pre>
+                </div>
+
+                <p>Beispiele:</p>
+
+                <div class="highlighter-rouge">
+                    <pre class="highlight">
+                        <code>OSR_BL_Da1_05_Eastside-Böblingen.pdf
+                        OSR_BL_He3_03_Hertha-Seligenstadt.pdf
+                        OSR_RL_DaNord_01_Füchse-Göttingen.pdf
+                        OSR_RL_HeNord_02_Düppel-Tündern.pdf
+                        OSR_OL_DaNordOst_12_Neukölln-Ahrensberg.pdf
+                        OSR_OL_HeNordOst_13_Spandau-Sasel.pdf
+                        </code>
+                    </pre>
+                </div>
+
+                <h2 id="fill">OSR-Bericht ausfüllen</h2>
+
+                <p>Beim Ausfüllen des OSR-Berichts gibt es zwei Phasen:</p>
+
+                <ol>
+                  <li>Daten erheben (beim Spiel)</li>
+                  <li>Daten in PDF-Formular übertragen (am Computer)</li>
+                </ol>
+
+                <p>Da ich selten einen Computer beim Spiel dabeihabe, drucke ich für jedes Spiel einen OSR-Bericht aus, den ich während des Spiels ausfülle.
+                    Diese Daten übernehme ich dann zu Hause in den Computer.
+                </p>
+
+                <p>Am Computer öffnet Ihr die PDF-Datei mit dem Acrobat Reader.
+                    Es gibt zwar gute und kostenlose Alternativen zum Adobe Reader (siehe 
+                    <a href="https://de.wikipedia.org/wiki/Liste_von_PDF-Software">Wikipedia</a>) 
+                    mit diesen funktionieren die neuen Formulare jedoch nicht immer.
+                    <em>PDFXChangeViewer</em> soll funktionieren.
+                </p>
+
+                <p>Nachdem Ihr die PDF-Datei geöffnet habt, tragt Ihr die Daten des Spiels in das Formular ein.</p>
+
+                <p>Um sicherzugehen, dass Ihr die Eintragungen abspeichern und wieder laden können, tragt zunächst nur wenige Daten ein, z.B. die Heimmannschaft.
+                    Speichert die Datei ab und schließt den PDF-Reader.
+                    Öffnet jetzt die gespeicherte PDF-Datei.
+                    Wenn die Heimmannschaft immer noch eingetragen ist, funktioniert das Speichern.
+                    Wenn nicht, verwendet einen anderen PDF-Reader.
+                </p>
+
+                <p>Das Formular ist relativ gut aufgebaut, daher nur ein paar wichtige Punkte, die oft falsch eingetragen werden:</p>
+
+                <ul>
+                    <li>Spielnummer nicht vergessen 
+                        <a href="/downloads/einsatzinformationen/BeTTV_Einsatzplan.pdf">Einsatzplan (pdf)</a> bzw. 
+                        <a href="/downloads/einsatzinformationen/BeTTV_Einsatzplan.xls">Einsatzplan (xls)</a>
+                    </li>
+                    <li>elektronisches Zählgerät in der Box: wirklich nur, wenn in der Box welche vorhanden waren, es ist nicht der Gesamtspielstandsanzeiger gemeint</li>
+                    <li>OSR-Lizenz ankreuzen</li>
+                    <li>Mannschaftsführer namentlich eintragen</li>
+                    <li>Kreuz bei “Gastverein hat das Trikot gewechselt…” nur dann setzen (ja oder nein), wenn die Trikots gleichfarbig waren. Im Normalfall hier also kein Kreuz (weder bei ja noch bei nein) setzen.</li>
+                    <li>Schläger müssen vor dem Spiel überprüft werden (Sichtprüfung)</li>
+                    <li>Messgerätprüfung nur, wenn man eins hat</li>
+                    <li>bei den Gründen für Karten: genaue Angabe, also nicht nur “Unsportlichkeit”, sondern “Treten gegen den Tisch”</li>
+                </ul>
+
+                <h2 id="save">OSR-Bericht abspeichern</h2>
+
+                <p>Jetzt speichert den OSR-Bericht ab. Da Ihr das oben schon getestet haben, sollten Bericht und Daten gespeichert sein.
+                    Ihr könnt das überprüfen, indem Ihr das Dokument schließt und wieder öffnet.
+                    Jetzt müssen die eingetragenen Daten noch vorhanden sein.
+                </p>
+
+                <p>Prüft bitte, ob die Daten nach einmal Programm schließen und wieder öffnen noch sichtbar sind, damit Ihr keinen leeren OSR-Bericht verschickt.</p>
+
+                <h2 id="send">OSR-Bericht per E-Mail versenden</h2>
+
+                <p>Der OSR-Bericht muss innerhalb von 48 Stunden an den jeweiligen Staffelleiter und an den VSRO (Ekkart Kleinod) versendet werden.</p>
+
+                <p>Prüft noch einmal, ob alle Daten eingetragen sind (einfach noch einmal durchlesen oder noch besser: jemandem zum Durchlesen geben).</p>
+
+                <p>Jetzt sendet den OSR-Bericht an alle Empfänger, wie das genau geschieht, hängt von Eurem PDF-Reader und Eurem Mailprogramm ab, so dass es schwierig ist, eine allgemeine Anleitung zu geben.
+                    Grundsätzlich verfasst Ihr eine neue E-Mail, tragt die E-Mailempfänger ein, hängt den OSR-Bericht an und schickt die E-Mail ab.
+                </p>
+
+                <h3 id="bundesliga-damen">1. Bundesliga Damen</h3>
+
+                <ul>
+                    <li>Gabi Klis: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#107;&#108;&#105;&#115;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;">&#107;&#108;&#105;&#115;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>André Zickert: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#122;&#105;&#099;&#107;&#101;&#114;&#116;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;">&#122;&#105;&#099;&#107;&#101;&#114;&#116;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+                <h3 id="bundesliga-herren-nord">3. Bundesliga Herren Nord</h3>
+
+                <ul>
+                    <li>Gabi Klis: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#107;&#108;&#105;&#115;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;">&#107;&#108;&#105;&#115;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>André Zickert: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#122;&#105;&#099;&#107;&#101;&#114;&#116;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;">&#122;&#105;&#099;&#107;&#101;&#114;&#116;&#046;&#100;&#116;&#116;&#098;&#064;&#116;&#105;&#115;&#099;&#104;&#116;&#101;&#110;&#110;&#105;&#115;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+                <h3 id="regionalliga-damen-nord">Regionalliga Damen Nord</h3>
+
+                <ul>
+                    <li>Andreas Ahlers: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;">&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+                <h3 id="regionalliga-herren-nord">Regionalliga Herren Nord</h3>
+
+                <ul>
+                    <li>Lothar Fricke: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#102;&#114;&#105;&#099;&#107;&#101;&#064;&#116;&#116;&#118;&#110;&#046;&#100;&#101;">&#102;&#114;&#105;&#099;&#107;&#101;&#064;&#116;&#116;&#118;&#110;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+                <h3 id="oberliga-nordost-damen">Oberliga Nordost Damen</h3>
+
+                <ul>
+                    <li>Andreas Ahlers: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;">&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+                <h3 id="oberliga-nordost-herren">Oberliga Nordost Herren</h3>
+
+                <ul>
+                    <li>Andreas Ahlers: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;">&#109;&#097;&#105;&#108;&#064;&#097;&#104;&#108;&#101;&#114;&#115;&#045;&#116;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                    <li>Ekkart Kleinod: 
+                        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;">&#115;&#099;&#104;&#105;&#114;&#105;&#064;&#101;&#107;&#107;&#097;&#114;&#116;&#046;&#100;&#101;</a>
+                    </li>
+                </ul>
+
+
+            </div>
+        </div>
+
+
+        <hr />
+
+        <?php include ("../footer.php"); ?>
+
+    </div>
+
+		<!-- jQuery -->
+    <script src="//code.jquery.com/jquery-2.2.0.min.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
