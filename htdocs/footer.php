5444e1fee684a16254227b4826a2e384dcec2b57
tkantusch <tobiaskantusch@online.de>
2016-10-02 00:22:03 +0200

    Wechsel zur Endung .php
    HTML in PHP geändert, um das Menü und den Footer auszulagern und somit die Verwaltung des Menüs und des Footers zu zentralisieren und zu vereinfachen

---
+++ htdocs/footer.php
@@ -0,0 +1,12 @@
+<header>
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>
+				    Letzte Änderung: 28. September 2016,
+				    Ekkart Kleinod
+				</p>
+            </div>
+        </div>
+    </footer>
+</header>
