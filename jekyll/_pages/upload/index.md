18478467a255d33680496a41552144442d39d2e6
Ekkart Kleinod <ekleinod@edgesoft.de>
2016-10-11 00:13:41 +0200

    upload-Bereich funktioniert, Einsatzpläne in umlautfreiem Verzeichnis

--- jekyll/_pages/upload/index.md
+++ jekyll/_pages/upload/index.md
@@ -13,16 +13,18 @@ hier bitte den neuen Einsatzplan hochladen <i class="fa fa-smile-o"></i>
 	<div class="control-group form-group">
 		<div class="controls">
 			<label>Einsatzplan (pdf)</label>
-			<input type="file" class="form-control" id="pdf" required="required">
+			<input type="file" class="form-control" id="pdf" name="pdf" required="required">
 		</div>
 	</div>
 	<div class="control-group form-group">
 		<div class="controls">
 			<label>Einsatzplan (xls)</label>
-			<input type="file" class="form-control" id="excel" required="required">
+			<input type="file" class="form-control" id="excel" name="excel" required="required">
 		</div>
 	</div>
 	<div id="success"></div>
 	<!-- For success/fail messages -->
-	<button type="submit" class="btn btn-primary">Hochladen</button>
+	<button type="submit" class="btn btn-primary" name="submit">Hochladen</button>
 </form>
+
+Anmerkung: bei jedem Abschicken bekomme ich eine E-Mail.
--- jekyll/_pages/upload/upload.php
+++ jekyll/_pages/upload/upload.php
@@ -5,58 +5,89 @@ image: banner/upload.png
 date: 09.10.2016
 ---
 
-Dann wollen wir mal...
+		<p>Dann wollen wir mal...</p>
 
-## PDF-Datei
+<h2>PDF-Datei</h2>
 
 <?php
-$target_dir = "uploads/";
+$upload_dir = "uploads/";
+$download_name = "../downloads/einsatzplaene/BeTTV_Einsatzplan.";
+$error = false;
+?>
 
-$target_file = $target_dir . basename($_FILES["pdf"]["name"]);
-$fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+<?php
+$upload_file = $upload_dir . basename($_FILES["pdf"]["name"]);
+$file_type = pathinfo($upload_file, PATHINFO_EXTENSION);
+$download_file = $download_name . $file_type;
 
 if (isset($_POST["submit"])) {
 
-	$isOK = ($fileType === "pdf");
+	$isOK = ($file_type === "pdf");
 
 	if ($isOK) {
 
-		if (move_uploaded_file($_FILES["pdf"]["tmp_name"], $target_file)) {
+		if (move_uploaded_file($_FILES["pdf"]["tmp_name"], $upload_file)) {
 			echo '<div class="alert alert-success"><strong>Erfolg!</strong> Die Datei wurde hochgeladen.</div>';
+
+			if (copy($upload_file, $download_file)) {
+				echo '<div class="alert alert-success"><strong>Erfolg!</strong> Die Datei wurde in den Download-Ordner kopiert.</div>';
+			} else {
+				echo '<div class="alert alert-danger"><strong>Fehler!</strong> Beim Kopieren in den Download-Ordner ist ein Fehler aufgetreten.</div>';
+			}
 		} else {
 			echo '<div class="alert alert-danger"><strong>Fehler!</strong> Beim Hochladen ist ein Fehler aufgetreten.</div>';
+			$error = true;
 		}
 
 	} else {
 		echo '<div class="alert alert-danger"><strong>Fehler!</strong> Die Datei ist keine gültige PDF-Datei und wurde abgelehnt.</div>';
+		$error = true;
 	}
 
+} else {
+	echo '<div class="alert alert-danger"><strong>Fehler!</strong> submit nicht gesetzt.</div>';
+	$error = true;
 }
 ?>
 
-## Excel-Datei
+<h2>Excel-Datei</h2>
 
 <?php
-$target_dir = "uploads/";
-
-$target_file = $target_dir . basename($_FILES["excel"]["name"]);
-$fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+$upload_file = $upload_dir . basename($_FILES["excel"]["name"]);
+$file_type = pathinfo($upload_file, PATHINFO_EXTENSION);
+$download_file = $download_name . $file_type;
 
 if (isset($_POST["submit"])) {
 
-	$isOK = ($fileType === "xls");
+	$isOK = ($file_type === "xls");
 
 	if ($isOK) {
 
-		if (move_uploaded_file($_FILES["excel"]["tmp_name"], $target_file)) {
+		if (move_uploaded_file($_FILES["excel"]["tmp_name"], $upload_file)) {
 			echo '<div class="alert alert-success"><strong>Erfolg!</strong> Die Datei wurde hochgeladen.</div>';
+
+			if (copy($upload_file, $download_file)) {
+				echo '<div class="alert alert-success"><strong>Erfolg!</strong> Die Datei wurde in den Download-Ordner kopiert.</div>';
+			} else {
+				echo '<div class="alert alert-danger"><strong>Fehler!</strong> Beim Kopieren in den Download-Ordner ist ein Fehler aufgetreten.</div>';
+			}
 		} else {
 			echo '<div class="alert alert-danger"><strong>Fehler!</strong> Beim Hochladen ist ein Fehler aufgetreten.</div>';
+			$error = true;
 		}
 
 	} else {
 		echo '<div class="alert alert-danger"><strong>Fehler!</strong> Die Datei ist keine gültige Excel-Datei und wurde abgelehnt.</div>';
+		$error = true;
 	}
 
+} else {
+	echo '<div class="alert alert-danger"><strong>Fehler!</strong> submit nicht gesetzt.</div>';
+	$error = true;
 }
+
+mail("schiri@ekkart.de", "Upload Schiriseiten", "Upload Einsatzpläne: " . (($error) ? "erfolglos" : "erfolgreich"));
+
 ?>
+
+<p><a href="index.html">Zurück zum Hochladen</a></p>
