8e13c6b149e4422bd3013de9516132f23ccfbd00
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-03-19 23:48:59 +0100

    SymLinks gesetzt

--- jekyll/js/local.js
+++ jekyll/js/local.js
@@ -1,6 +1,7 @@
+/*
+* Empty file, so there are no errors if you do not have a local file.
+*/
 
-$("[data-toggle='sidebar']").click(function() {
-    var selector = $(this).data("target");
-    $(selector).toggleClass('show');
-    $(this).toggleClass("move");
-});
+/* your definitions here.
+
+/* EOF */
